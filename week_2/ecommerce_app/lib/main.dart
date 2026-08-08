3ef945441fdb03234217b17d9a1f92b843fb9da6
yoadin <kalkidanasdro11@gmail.com>
2026-08-08 10:19:15 +0300

    Configure ecommerce app entry point

---
+++ week_2/ecommerce_app/lib/main.dart
@@ -0,0 +1,47 @@
+﻿import 'package:ecommerce_app/core/constants/app_constants.dart';
+import 'package:ecommerce_app/core/router/app_router.dart';
+import 'package:ecommerce_app/core/theme/app_theme.dart';
+import 'package:ecommerce_app/features/auth/presentation/providers/auth_provider.dart';
+import 'package:ecommerce_app/features/cart/data/models/cart_item_model.dart';
+import 'package:ecommerce_app/features/products/data/models/product_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:ecommerce_app/core/utils/root_messenger.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(CartItemModelAdapter());
+  Hive.registerAdapter(ProductModelAdapter());
+  await Hive.openBox<CartItemModel>(AppConstants.cartBox);
+  await Hive.openBox<ProductModel>(AppConstants.productsBox);
+
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      child: const EcommerceApp(),
+    ),
+  );
+}
+
+class EcommerceApp extends ConsumerWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      title: 'ShopSphere',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      scaffoldMessengerKey: rootScaffoldMessengerKey,
+      routerConfig: router,
+    );
+  }
+}
