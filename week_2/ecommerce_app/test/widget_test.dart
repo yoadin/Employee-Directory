9de2b1b80a695dcf0ca40750b0227573a8965753
yoadin <kalkidanasdro11@gmail.com>
2026-08-08 10:31:31 +0300

    Add ecommerce app tests

---
+++ week_2/ecommerce_app/test/widget_test.dart
@@ -0,0 +1,31 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:ecommerce_app/features/auth/presentation/providers/auth_provider.dart';
+import 'package:ecommerce_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  testWidgets('Ecommerce app loads smoke test', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+        child: const EcommerceApp(),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
